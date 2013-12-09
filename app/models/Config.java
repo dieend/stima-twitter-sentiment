c1668dd74f78bb06abf318426b1092d277ca22f4
dieend <mail.dieend@gmail.com>
2013-12-09 15:04:25 +0700

    config.java

---
+++ app/models/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Okihita
+ */
+public class Config {
+    public static final String ACCESS_TOKEN = "173155612-k3mGeLLuvWHD9sfU9LsrOUnnIuyjMer59Q6AfcYq";
+    public static final String ACCESS_TOKEN_SECRET = "uj7oh5HPAuPHVJL7KnBZsZx5sNDnPItzMgdQthL9QfvRe";
+    public static final String CONSUMER_KEY = "lDSXaADxfsaGWkM7apBWg";
+    public static final String CONSUMER_KEY_SECRET = "YLqz3V4VxIVEXXqZtWhoHN3Uwz25vicM4t5of5DuI";    
+}
